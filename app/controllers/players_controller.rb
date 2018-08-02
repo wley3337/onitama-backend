56e1cc46be0d871c247e54962d8ebc08db0e98c8
github username <github email address>
2018-08-02 09:32:57 -0400

    added update method to players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  
+
   def index
     render json: Player.all
   end
@@ -10,13 +10,12 @@ class PlayersController < ApplicationController
 
   def reset
     Player.reset
-  end 
+  end
 
-  # FOR IF WE WANT TO UPDATE NAME
-  # def update
-  #   player = Player.find(params[:id])
-  #   player.update(params.require(:player).permit(:name))
-  #   render json: player
-  # end
+  def update
+    player = Player.find(params[:id])
+    player.update(params.require(:player).permit(:active_player))
+    render json: player
+  end
 
 end
